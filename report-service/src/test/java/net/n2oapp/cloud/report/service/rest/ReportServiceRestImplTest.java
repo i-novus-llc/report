9ae429a4300d797f8c7bf32f263fbafd7d50696f
bkhafizullin <bhafizullin@i-novus.ru>
2020-12-02 12:30:03 +0300

    nf REPENG-9 покрытие тестами сервиса отчетов

--- report-service/src/test/java/net/n2oapp/cloud/report/service/rest/ReportServiceRestImplTest.java
+++ report-service/src/test/java/net/n2oapp/cloud/report/service/rest/ReportServiceRestImplTest.java
@@ -1,9 +1,9 @@
 package net.n2oapp.cloud.report.service.rest;
 
 import junit.framework.TestCase;
-import net.n2oapp.platform.test.autoconfigure.DefinePort;
 import net.n2oapp.cloud.report.api.ReportService;
 import net.n2oapp.cloud.report.service.ReportApplication;
+import net.n2oapp.platform.test.autoconfigure.DefinePort;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.ContentDisposition;
 import org.junit.After;
@@ -15,15 +15,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
 import javax.ws.rs.core.*;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
@@ -42,12 +44,61 @@ public class ReportServiceRestImplTest extends TestCase {
     private static final String PATH = "/net/n2oapp/report/service/rest/";
     private static final String MASTER_TEMPLATE_FILE_NAME = "masterReportTemplate";
     private static final String DETAIL_TEMPLATE_FILE_NAME = "detailReportTemplate";
+    private static final String EXAMPLE_TEMPLATE_FILE_NAME = "exampleReportTemplate";
 
     @Value("${fileStorage.root}")
     private String fileStorageRoot;
 
     @Autowired
     private ReportService reportService;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testGetReportFromInMemoryDb() throws Exception {
+        initDataForReport();
+        testCompile(EXAMPLE_TEMPLATE_FILE_NAME);
+
+        UriInfo uriInfo = Mockito.mock(UriInfo.class);
+        MultivaluedMap queryParamMap = new MultivaluedHashMap();
+        queryParamMap.put("REPORT_DATASOURCE_NAME", List.of("dataSource"));
+        Mockito.when(uriInfo.getQueryParameters()).thenReturn(queryParamMap);
+
+        try (Response response = reportService.generateReport(EXAMPLE_TEMPLATE_FILE_NAME, "csv", uriInfo)) {
+            assertEquals(response.getStatus(), 200);
+            assertTrue(response.getHeaders().get(HttpHeaders.CONTENT_DISPOSITION).get(0).toString().contains(EXAMPLE_TEMPLATE_FILE_NAME + "." + "csv"));
+            compareContents(response);
+        }
+
+    }
+
+    private void initDataForReport() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        connection.createStatement().execute(getSqlStatement());
+        connection.close();
+    }
+
+    private void compareContents(Response response) {
+        ByteArrayInputStream inputStream = (ByteArrayInputStream) response.getEntity();
+        String[] employees = new String(inputStream.readAllBytes()).split("\n");
+
+        /*
+        report content starts with zero width no-break space (ZWNBSP), a deprecated use of the Unicode character at code point U+FEFF
+        first string has this symbol
+         */
+        assertTrue(employees[0].contains("Michael"));
+
+        assertEquals("25", employees[1]);
+        assertEquals("1000", employees[2]);
+        assertEquals("John", employees[3]);
+        assertEquals("30", employees[4]);
+        assertEquals("1500", employees[5]);
+        assertEquals("Tom", employees[6]);
+        assertEquals("36", employees[7]);
+        assertEquals("2000", employees[8]);
+    }
+
 
     @Test
     @SuppressWarnings("unchecked")
@@ -63,11 +114,11 @@ public class ReportServiceRestImplTest extends TestCase {
         testGenerate("pdf", uriInfo);
         testGenerate("xml", uriInfo);
         testGenerate("csv", uriInfo);
-        testGenerate( "xls", uriInfo);
-        testGenerate( "xlsx", uriInfo);
-        testGenerate( "docx", uriInfo);
-        testGenerate( "odt", uriInfo);
-        testGenerate( "ods", uriInfo);
+        testGenerate("xls", uriInfo);
+        testGenerate("xlsx", uriInfo);
+        testGenerate("docx", uriInfo);
+        testGenerate("odt", uriInfo);
+        testGenerate("ods", uriInfo);
     }
 
     private void testCompile(String templateFileName) throws IOException {
@@ -84,6 +135,12 @@ public class ReportServiceRestImplTest extends TestCase {
             assertEquals(response.getStatus(), 200);
             assertTrue(response.getHeaders().get(HttpHeaders.CONTENT_DISPOSITION).get(0).toString().contains(MASTER_TEMPLATE_FILE_NAME + "." + format));
         }
+    }
+
+    private String getSqlStatement() {
+        return new BufferedReader(
+                new InputStreamReader(ReportServiceRestImplTest.class.getResourceAsStream("/script/createEmployees.sql")))
+                .lines().collect(Collectors.joining("\n"));
     }
 
     @After
